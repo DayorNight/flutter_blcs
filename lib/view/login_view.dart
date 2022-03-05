7b83e92ae5ab8a02398036529f8fa78c727b642c
linweibin <Lwb123456>
2022-03-05 19:23:12 +0800

    【flutter】登录界面

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -9,16 +9,19 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  GlobalKey _key = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text("登陆"),
         centerTitle: true,
         elevation: 10,
       ),
       body: Padding(
-        padding: EdgeInsets.all(20),
+        padding: EdgeInsets.fromLTRB(20, 40, 20, 20),
         child: Column(
           children: [
             Center(
@@ -31,21 +34,87 @@ class _LoginViewState extends State<LoginView> {
             ),
             SizedBox(height: 16),
             Form(
+              key: _key,
               child: Column(
                 children: [
                   TextFormField(
                     keyboardType: TextInputType.phone,
                     decoration: const InputDecoration(
-                      hintText: '输入账号'
+                      counterText:'',//隐藏最大长度字数
+                      hintText: '请输入手机号',
+                      labelText: '账号',
+                      prefixIcon: Icon(Icons.person),
                     ),
                     validator: (String? value){
                       if(value == null || value.isEmpty){
-                        return '未输入账号';
+                        return '未输入手机号';
                       }
-                      return null;
                     },
-                  )
+                    textInputAction: TextInputAction.next,
+                    maxLength: 11,
+                      enableSuggestions:false
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.text,
+                    decoration: const InputDecoration(
+                      hintText: '请输入密码',
+                      labelText: '密码',
+                      prefixIcon: Icon(Icons.lock),
+                    ),
+                    validator: (String? value){
+                      if(value == null || value.isEmpty){
+                        return '未输入密码';
+                      }
+                      if(value.length<6){
+                        return '密码不能小于6位数';
+                      }
+                    },
+                    obscureText: true,
+                    textInputAction: TextInputAction.send,
+                    onFieldSubmitted:  (String){
+                      print('submit');
+                    },
+                  ),
                 ],
+              ),
+            ),
+            SizedBox(height: 16),
+            GestureDetector(
+              child: Container(
+                width: double.infinity,
+                child: Text("忘记密码",
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              onTap: (){
+                print("忘记密码");
+                // Navigator.of(context).pushNamed("forgetPsd");
+              },
+            ),
+            SizedBox(height: 16),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: (){
+                  // (_key.currentState as FormState).toString();
+                  print((_key.currentState as FormState).validate());
+                  // print("登录");
+                  // Navigator.of(context).pushNamed("forgetPsd");
+                },
+                child: Text("登录"),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: (){
+                  print("注册");
+                  // Navigator.of(context).pushNamed("forgetPsd");
+                },
+                  child: Text("注册")
               ),
             ),
           ],
